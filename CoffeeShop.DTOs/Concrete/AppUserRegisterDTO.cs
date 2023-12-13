b63c39ff8056f86cfaf7a6f10fb4deb998064ac5
OzanUlus <ilkerozanulus@gmail.com>
2023-12-14 00:52:14 +0300

    fixed registerdto

--- CoffeeShop.DTOs/Concrete/AppUserRegisterDTO.cs
+++ CoffeeShop.DTOs/Concrete/AppUserRegisterDTO.cs
@@ -14,6 +14,7 @@ namespace CoffeeShop.DTOs.Concrete
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string RepeatPassword { get; set; }
         public string County { get; set; }
         public string Address { get; set; }
         public City City { get; set; }
