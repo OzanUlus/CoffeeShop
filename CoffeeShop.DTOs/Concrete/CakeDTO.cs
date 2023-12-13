56dc6e4dd58ea07e13c2e5ad35d0e44c2856b455
OzanUlus <ilkerozanulus@gmail.com>
2023-12-14 00:57:07 +0300

    added cakedto

---
+++ CoffeeShop.DTOs/Concrete/CakeDTO.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.DTOs.Concrete
+{
+    internal class CakeDTO : IDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Describe { get; set; }
+        public decimal Price { get; set; }
+    }
+}
