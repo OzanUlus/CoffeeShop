e996010f059561e9ffeff4e46b09253cd4ca2d09
OzanUlus <ilkerozanulus@gmail.com>
2023-12-14 01:11:38 +0300

    added orderdto

---
+++ CoffeeShop.DTOs/Concrete/OrderDTO.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeShop.Entity.Concrete;
+using CoffeeShop.Entity.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.DTOs.Concrete
+{
+    public class OrderDTO
+    {
+        public string OrderNumber { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public OrderStates OrderState { get; set; }
+        public virtual ICollection<OrderLine> OrderLines { get; set; }
+
+    }
+}
