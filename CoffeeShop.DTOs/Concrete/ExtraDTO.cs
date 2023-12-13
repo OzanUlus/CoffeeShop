636643969bc7f5e6c9af22794f6ba246c4f1d0f7
OzanUlus <ilkerozanulus@gmail.com>
2023-12-14 01:04:24 +0300

    added extradto

---
+++ CoffeeShop.DTOs/Concrete/ExtraDTO.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.DTOs.Concrete
+{
+    public class ExtraDTO : IDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal AdditionPrice { get; set; }
+    }
+}
