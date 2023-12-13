85c48f0e905ce8b59b0a4ab88b436136f36c650e
OzanUlus <ilkerozanulus@gmail.com>
2023-12-14 01:09:38 +0300

    added orderlinedto

---
+++ CoffeeShop.DTOs/Concrete/OrderLineDTO.cs
@@ -0,0 +1,24 @@
+﻿using CoffeeShop.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.DTOs.Concrete
+{
+    public class OrderLineDTO
+    {
+        public string Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public virtual Order Order { get; set; }
+        public string OrderId { get; set; }
+
+        public AppUser AppUser { get; set; }
+        public string AppUserId { get; set; }
+        public string? AppUserNameAndSurname { get; set; }
+        public ICollection<Extra> Extras { get; set; }
+
+    }
+}
